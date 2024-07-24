31e0654335f952f00d40dd1f69991f9fa342a9f4
hassanfawazdipo <150442083+hassanfawazdipo@users.noreply.github.com>
2024-07-24 14:17:27 +0100

    Add files via upload

---
+++ stellarcar.js
@@ -0,0 +1,257 @@
+function icon(){
+    var icon = document.getElementById("nav");
+    icon.classList.toggle("active");
+}
+
+const brands ="Mecedeze Benz";
+const  models = "Mecedeze Benz";
+const price = "$200,000"
+const year = "2022";
+const chasen = "FD12354";
+const colors ="red";
+const seattype = " leather";
+const manucountry ="Germany";
+const enginetype ="1.6";
+
+
+const car = {
+Brand:brands, 
+Model:models, 
+price: price,
+Year:year, 
+ChaseN:chasen,
+Color:colors, 
+SeatType:seattype,
+}
+
+function myBusiness(){
+    document.getElementById("benz").innerHTML= "brand : " + brands;
+    document.getElementById("benz1").innerHTML= "price : " + price;
+    document.getElementById("benz2").innerHTML= "year : " + year;
+    document.getElementById("benz3").innerHTML= "seat type : " + seattype;
+    document.getElementById("benz4").innerHTML= "color : " + colors;
+}
+
+const brands2 ="Rolls-Royce";
+const  models2 = "Rolls-Royce";
+const price2 = "$500,000"
+const year2 = "2024";
+const chasen2 = "FD12374";
+const colors2 ="white";
+const seattype2 = " leather";
+const manucountry2 ="Germany";
+const enginetype2 ="2.6";
+
+
+const car2 ={
+Brand:brands2, 
+Model:models2, 
+price:price2,
+Year:year2, 
+ChaseN:chasen2,
+Color:colors2, 
+SeatType:seattype2,
+}
+
+function myBusiness1(){
+    document.getElementById("rols").innerHTML= "brand : " + brands2;
+    document.getElementById("rols1").innerHTML= "price : " + price2;
+    document.getElementById("rols2").innerHTML= "year : " + year2;
+    document.getElementById("rols3").innerHTML= "seat type : " + seattype2;
+    document.getElementById("rols4").innerHTML= "color : " + colors2;
+}
+
+const brands3 ="Ferrari";
+const  models3 = "Ferrari";
+const price3 = "$250,000"
+const year3 = "2023";
+const chasen3 = "FD42354";
+const colors3 ="red";
+const seattype3 = " leather";
+const manucountry3 ="Germany";
+const enginetype3 ="2.3";
+
+
+const car3 = {
+Brand:brands3, 
+Model:models3, 
+price: price3,
+Year:year3, 
+ChaseN:chasen3,
+Color:colors3, 
+SeatType:seattype3,
+}
+
+function myBusiness2(){
+    document.getElementById("fer").innerHTML= "brand : " + brands3;
+    document.getElementById("fer1").innerHTML= "price : " + price3;
+    document.getElementById("fer2").innerHTML= "year : " + year3;
+    document.getElementById("fer3").innerHTML= "seat type : " + seattype3;
+    document.getElementById("fer4").innerHTML= "color : " + colors3;
+}
+
+const brands4 ="Acura";
+const  models4 = "NSX Type S";
+const price4 = "$100,000"
+const year4 = "2022";
+const chasen4 = "FD12354";
+const colors4 ="rose gold ";
+const seattype4 = " leather";
+const manucountry4 ="italy";
+const enginetype4 ="2.0";
+
+
+const car4 = {
+Brand:brands4, 
+Model:models4, 
+price: price4,
+Year:year4, 
+ChaseN:chasen4,
+Color:colors4, 
+SeatType:seattype4,
+}
+
+function myBusiness3(){
+    document.getElementById("acu").innerHTML= "brand : " + brands4;
+    document.getElementById("acu1").innerHTML= "price : " + price4;
+    document.getElementById("acu2").innerHTML= "year : " + year4;
+    document.getElementById("acu3").innerHTML= "seat type : " + seattype4;
+    document.getElementById("acu4").innerHTML= "color : " + colors4;
+}
+
+const brands5 ="blue bittle";
+const  models5 = "blue bittle";
+const price5 = "$220,000"
+const year5 = "2022";
+const chasen5 = "FD12774";
+const colors5 ="blue";
+const seattype5 = " leather";
+const manucountry5 ="china";
+const enginetype5 ="1.6";
+
+
+const car5 = {
+Brand:brands5, 
+Model:models5, 
+price: price5,
+Year:year5, 
+ChaseN:chasen5,
+Color:colors5, 
+SeatType:seattype5,
+}
+
+function myBusiness4(){
+    document.getElementById("blue").innerHTML= "brand : " + brands5;
+    document.getElementById("blue1").innerHTML= "price : " + price5;
+    document.getElementById("blue2").innerHTML= "year : " + year5;
+    document.getElementById("blue3").innerHTML= "seat type : " + seattype5;
+    document.getElementById("blue4").innerHTML= "color : " + colors5;
+}
+
+const brands6 ="jeep";
+const  models6 = "jeep";
+const price6 = "$80,000"
+const year6 = "2022";
+const chasen6 = "FD12354";
+const colors6 ="white";
+const seattype6 = " leather";
+const manucountry6 ="india";
+const enginetype6 ="1.6";
+
+
+const car6 = {
+Brand:brands6, 
+Model:models6, 
+price: price6,
+Year:year6, 
+ChaseN:chasen6,
+Color:colors6, 
+SeatType:seattype6,
+}
+
+function myBusiness5(){
+    document.getElementById("suv").innerHTML= "brand : " + brands6;
+    document.getElementById("suv1").innerHTML= "price : " + price6;
+    document.getElementById("suv2").innerHTML= "year : " + year6;
+    document.getElementById("suv3").innerHTML= "seat type : " + seattype6;
+    document.getElementById("suv4").innerHTML= "color : " + colors6;
+}
+
+const brands7 ="black bittle";
+const  models7 = "black bittle";
+const price7 = "$900,000";
+const year7 = "2022";
+const chasen7 = "FD12354";
+const colors7 ="black";
+const seattype7 = " leather";
+const manucountry7 ="Germany";
+const enginetype7 ="1.6";
+
+
+const car7 = {
+Brand:brands7, 
+Model:models7, 
+price: price7,
+Year:year7, 
+ChaseN:chasen7,
+Color:colors7, 
+SeatType:seattype7,
+}
+
+function myBusiness6(){
+    document.getElementById("bet").innerHTML= "brand : " + brands7;
+    document.getElementById("bet1").innerHTML= "price : " + price7;
+    document.getElementById("bet2").innerHTML= "year : " + year7;
+    document.getElementById("bet3").innerHTML= "seat type : " + seattype7;
+    document.getElementById("bet4").innerHTML= "color : " + colors7;
+}
+
+const brands8 ="red bittle";
+const  models8 = "red bittle";
+const price8 = "$1,200,000";
+const year8 = "2022";
+const chasen8 = "FD12354";
+const colors8 ="red";
+const seattype8 = " leather";
+const manucountry8 ="Germany";
+const enginetype8 ="1.6";
+
+
+const car8 = {
+Brand:brands8, 
+Model:models8, 
+price: price8,
+Year:year8, 
+ChaseN:chasen8,
+Color:colors8, 
+SeatType:seattype8,
+}
+
+function myBusiness7(){
+    document.getElementById("red").innerHTML= "brand     : " + brands8;
+    document.getElementById("red1").innerHTML= "price : " + price8;
+    document.getElementById("red2").innerHTML= "year : " + year8;
+    document.getElementById("red3").innerHTML= "seat type : " + seattype8;
+    document.getElementById("red4").innerHTML= "color : " + colors8;
+}
+
+
+function myStar() {
+    document.getElementById("color").style.color= "gold";
+}
+
+function myStar1() {
+    document.getElementById("color1").style.color= "gold";
+}
+
+function myStar2() {
+    document.getElementById("color2").style.color= "gold";
+}
+
+function myStar3() {
+    document.getElementById("color3").style.color= "gold";
+}
+
+function myStar4() {
+    document.getElementById("color4").style.color= "gold";
+}
